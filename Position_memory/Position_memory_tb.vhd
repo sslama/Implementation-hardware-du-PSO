efa7b45c068e484e7ddf2eed6fe690b6b5c567f7
sslama <safaslema1@gmail.com>
2019-07-15 02:18:46 +0200

    modification dans la taille de la memoire ajout des commentaires

--- Position_memory/Position_memory_tb.vhd
+++ Position_memory/Position_memory_tb.vhd
@@ -2,8 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_unsigned.all;    --used to do operations on vectors
-use IEEE.std_logic_misc.all;        --maybe not necessary
+use IEEE.std_logic_unsigned.all;    
+use IEEE.std_logic_misc.all;       
 USE IEEE.numeric_std.ALL;
 use IEEE.std_logic_arith.all;
 library work;
@@ -17,14 +17,14 @@ architecture Behavioral of position_memory_tb is
     port
         (        clk     : in  std_logic;
                  WR      : in  std_logic;
-                 address : in  std_logic_vector (3 downto 0);
+                 address : in  std_logic_vector (8 downto 0);
                  particule_in  : in array8;
                  particule_out : out array8 );
   end component; 
    --Inputs
     signal  clk: std_logic := '0';
     signal  WR: std_logic := '0';
-    signal  address:  std_logic_vector (3 downto 0);
+    signal  address:  std_logic_vector (8 downto 0);
     signal  particule_in : array8 ;
 
     --outputs 
@@ -56,7 +56,7 @@ architecture Behavioral of position_memory_tb is
 stim_proc: process
         begin        
             WR <= '1'; 
-            address <= "0010";
+            address <= "000000010";
             particule_in(0)<= "00010000";
             particule_in(1)<= "00000010";
             particule_in(2)<= "01000000";
@@ -64,7 +64,7 @@ stim_proc: process
             
             
             WR <= '1'; 
-            address <= "0110";
+            address <= "000000110";
             particule_in(0)<= "00010010";
             particule_in(1)<= "00000011";
             particule_in(2)<= "01000011";
@@ -73,11 +73,11 @@ stim_proc: process
            wait for 100 ns;
            
             WR <= '0'; 
-            address <= "0010";
+            address <= "000000010";
             wait for 100 ns;
            
              WR <= '0'; 
-             address <= "0110";
+             address <= "000000110";
              wait for 100 ns;
                        
            wait;
--- Position_memory/position_memory.vhd
+++ Position_memory/position_memory.vhd
@@ -1,53 +1,44 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
---define an array of 8bit std_logic_vector
-package pkg is
+
+package pkg is                                                    -- le package pour creer un nouveau type 
   type array8 is array (0 to 2) of std_logic_vector (7 downto 0); --tableau contenant 3 coordonnees codees sur 8bits (coordonnees de la particule)
-  type array_array is array (0 to 333) of array8; --tableau de RAM contenant les particules PBest
+  type array_array is array (0 to 1000) of array8;                --tableau de RAM contenant les 1000 positions mises a jour des particules 
 end package;
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_unsigned.all;    --used to do operations on vectors
-use IEEE.std_logic_misc.all;        --maybe not necessary
+use IEEE.std_logic_unsigned.all;    
+use IEEE.std_logic_misc.all;       
 USE IEEE.numeric_std.ALL;
 use IEEE.std_logic_arith.all;
 library work;
 use work.pkg.all;
 
 entity PM is
-generic (
-        num_particules : integer := 3 --number of particules
-    );
   port (
     clk     : in  std_logic;
-    WR      : in  std_logic;
-    address : in  std_logic_vector (3 downto 0);
-    particule_in  : in array8;
-    particule_out : out array8
+    WR      : in  std_logic;                        --Write/Read
+    address : in  std_logic_vector (8 downto 0);    --adresses dans la memoire 
+    particule_in  : in array8;                      -- position de la particule en entree ( 3d codee sur 8 bits) 
+    particule_out : out array8                      -- position de la particule en sortie ( 3d codee sur 8 bits) 
   );
 end entity PM;
 
 architecture RTL of PM is
-
-   signal myRam_particule : array_array;
-   signal sign_particule_in : array8;
-   signal sign_particule_out : array8;      
-
+   signal myRam_particule : array_array;  
 begin
 
 process (clk)
     begin
         if (clk'event and clk = '1') then
-            if (WR = '1') then
+            if (WR = '1') then             -- pour ecrire dans la memoire 
               myRam_particule (conv_integer(address)) <= particule_in ;
-             else 
+             else                          --pour lire de la memoire 
               particule_out <= myRam_particule(conv_integer(address));
             end if;
-            --particule_out <= sign_particule_out;
-            --fitness_out <= sign_fitness_out;
         end if;
     end process;
     
